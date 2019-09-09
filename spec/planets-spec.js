f9c3f80041887b1aef350ee08c627a5f97ae02c4
Erin Deuel <end.gaia@gmail.com>
2019-09-08 18:52:53 -0700

    add another test

--- spec/planets-spec.js
+++ spec/planets-spec.js
@@ -13,4 +13,15 @@ describe('Age', function(){
     expect(newUser.mars).toEqual(18);
     expect(newUser.jupiter).toEqual(3);
   });
+  it('should test if the life expectancy is being converted', function(){
+    let second = new Age(34, "female");
+    second.mercuryExpect();
+    second.venusExpect();
+    second.marsExpectancy();
+    second.jupiterExpect();
+    expect(second.mercExpect).toEqual(338);
+    expect(second.venExpect).toEqual(131);
+    expect(second.marsExpect).toEqual(43);
+    expect(second.jupExpect).toEqual(7);
+  });
 });
--- src/planets.js
+++ src/planets.js
@@ -1,44 +1,51 @@
 export function Age(earth, sex, mercury, venus, mars, jupiter, mercExpect, venExpect, marsExpect, jupExpect, mercOver, venOver, marsOver, jupOver){
+// current age on all planets
   this.earth = earth;
   this.mercury = mercury;
   this.venus = venus;
   this.mars = mars;
   this.jupiter = jupiter;
+  // gender determins life expectancy
   this.sex = sex;
   this.female = 81;
   this.male = 76;
+  // the difference between earth years and planet years
   this.mercNum = .24;
   this.venNum = .62;
   this.marsNum = 1.88;
   this.jupNum = 11.86;
+  // life expectency converted to all planet years
   this.mercExpect = mercExpect;
   this.venExpect = venExpect;
   this.marsExpect = marsExpect;
   this.jupExpect = jupExpect;
+  // if the user is over the expectancy, this tells you by how much
   this.mercOver = mercOver;
   this.venOver = venOver;
   this.marsOver = marsOver;
   this.jupOver = jupOver;
 }
+// coversion to mercury years
 Age.prototype.mercCalc = function() {
   this.mercury = Math.round(this.earth / this.mercNum);
   return this.mercury;
 };
-
+// conversion to venus years
 Age.prototype.venusCalc = function() {
   this.venus = Math.round(this.earth / this.venNum);
   return this.venus;
 };
-
+// conversion to mars years
 Age.prototype.marsCalc = function() {
   this.mars = Math.round(this.earth / this.marsNum);
   return this.mercury;
 };
-
+// conversion to jupiter years
 Age.prototype.jupCalc = function() {
   this.jupiter = Math.round(this.earth / this.jupNum);
   return this.jupiter;
 };
+// life expectancy for males and females on mercury
 Age.prototype.mercuryExpect = function() {
   if (this.sex === "male") {
     this.mercExpect = Math.round(this.male / this.mercNum);
@@ -49,6 +56,7 @@ Age.prototype.mercuryExpect = function() {
     return this.mercExpect;
   }
 };
+// life expectacy for males and females on venus
 Age.prototype.venusExpect = function(){
   if (this.sex === "male") {
     this.venExpect = Math.round(this.male / this.venNum);
@@ -59,6 +67,7 @@ Age.prototype.venusExpect = function(){
     return this.venExpect;
   }
 };
+// life expectancy for males and females on mars
 Age.prototype.marsExpectancy = function() {
   if (this.sex === "male") {
     this.marsExpect = Math.round(this.male / this.marsNum);
@@ -69,6 +78,7 @@ Age.prototype.marsExpectancy = function() {
     return this.marsExpect;
   }
 };
+// life expectacy for males and females on jupiter
 Age.prototype.jupiterExpect = function () {
   if (this.sex === "male") {
     this.jupExpect = Math.round(this.male / this.jupNum);
@@ -79,27 +89,43 @@ Age.prototype.jupiterExpect = function () {
     return this.jupExpect;
   }
 };
+// over expectacy mercury
 Age.prototype.over1 = function(){
   if(this.mercury > this.mercExpect){
     this.mercOver = this.mercury - this.mercExpect;
     return this.mercOver;
+  } else {
+    this.mercOver = 0;
+    return this.mercOver;
   }
 };
+// over expectacy venus
 Age.prototype.over2 = function(){
   if(this.venus > this.venExpect){
     this.venOver = this.venus - this.venExpect;
     return this.venOver;
+  } else {
+    this.venOver = 0;
+    return this.venOver;
   }
 };
+// over expectacy mars
 Age.prototype.over3 = function(){
   if(this.mars > this.marsExpect){
     this.marsOver = this.mars - this.marsExpect;
     return this.marsOver;
+  } else {
+    this.marsOver = 0;
+    return this.marsOver;
   }
 };
+// over expectacy jupiter
 Age.prototype.over4 = function(){
-  if(this.jupiter >= this.jupExpect){
+  if(this.jupiter > this.jupExpect){
     this.jupOver = this.jupiter - this.jupExpect;
+    return this.jupOver;
+  } else {
+    this.jupOver = 0;
     return this.jupOver;
   }
 };
